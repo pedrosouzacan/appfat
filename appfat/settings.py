cf6f3c72d71e18597d944afb47a78733fda282d5
Pedro Victor santos de Souza <pedro.souza@discentes.fat.uerj.br>
2023-09-19 21:03:18 -0300

    Rest Framework golbal configs update

--- appfat/settings.py
+++ appfat/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        #  'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
@@ -153,9 +153,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # autentificação para iniciar sessão:
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
 
         # autentificação via Token:
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
     )
 }
